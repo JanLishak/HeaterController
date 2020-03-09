34b4ea6caf087fe1e55842c7018029f49a79bb8e
Jan Lishak <35602700+JanLishak@users.noreply.github.com>
2020-03-09 20:16:58 +0100

    sorting the mess in files caused by moving files to new project(removing extra files)

--- HeaterControllerJavaProject/src/heating/utility/NamedPropertyChangeSubject.java
+++
@@ -1,9 +0,0 @@
-package heating.utility;
-
-import java.beans.PropertyChangeListener;
-
-public interface NamedPropertyChangeSubject
-{
-  void addListener(String propertyName, PropertyChangeListener listener);
-  void removeListener(String propertyName, PropertyChangeListener listener);
-}
--- HeaterControllerJavaProject/src/heating/utility/UnnamedPropertyChangeSubject.java
+++
@@ -1,9 +0,0 @@
-package heating.utility;
-
-import java.beans.PropertyChangeListener;
-
-public interface UnnamedPropertyChangeSubject
-{
-    void addListener(PropertyChangeListener listener);
-    void removeListener(PropertyChangeListener listener);
-}
